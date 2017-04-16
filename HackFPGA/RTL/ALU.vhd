955a598bb10605b669a748fe03805e8b16a81c0f
spillow <scott.pillow@gmail.com>
2017-04-16 16:28:18 -0700

    added ALU stub.

---
+++ HackFPGA/RTL/ALU.vhd
@@ -0,0 +1,23 @@
+---------------------------------------------------------------------------------- 
+-- Description: 
+-- 
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+entity ALU is
+--  Port ( );
+end ALU;
+
+architecture behavior of ALU is
+
+begin
+
+end behavior;
