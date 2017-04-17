9085d796456107c1fd47f0d824109eed2ec0777c
spillow <scott.pillow@gmail.com>
2017-04-16 23:25:53 -0700

    removed comment, updated gitignore for .runs.

--- HackFPGA/RTL/CPU.vhd
+++ HackFPGA/RTL/CPU.vhd
@@ -8,10 +8,6 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
 entity CPU is
   port (
     clk         : in  std_logic;
